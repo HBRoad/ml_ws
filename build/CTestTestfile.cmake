1a1ece0133941baf23411c064afc545061a19e4b
borninfreedom <ericwen657@foxmail.com>
2020-05-31 13:48:18 +0800

    description

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/dell/ml_ws/src
+# Build directory: /home/dell/ml_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(robotiq/robotiq_2f_140_gripper_visualization)
+subdirs(ur3/ur_description)
+subdirs(robotiq/robotiq_ft_sensor)
+subdirs(ur3/ur_moveit_config)
---
+++ build/ur3/ur_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dell/ml_ws/src/ur3/ur_description
+# Build directory: /home/dell/ml_ws/build/ur3/ur_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
